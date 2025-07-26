93d6f7a124e49dba561c3510d6df9accab026b89
THE-AUDIO <nandraina.dev22@gmail.com>
2025-07-26 11:05:52 +0300

    init chat

--- src/app/chat/main/main.component.ts
+++ src/app/chat/main/main.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
